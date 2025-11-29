0261bfa2ae5d9e84f761caf42b78af6709208803
dima <dima@HOME-PC>
2025-11-29 21:53:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp32/Program.cs
@@ -0,0 +1,37 @@
+﻿
+class Instrument
+{
+    public virtual void Play()
+    {
+        Console.WriteLine("Играет музыка...");
+    }
+}
+
+class Guitar : Instrument
+{
+    public override void Play()
+    {
+        Console.WriteLine("Брень-брень!");
+    }
+}
+
+class Drum : Instrument
+{
+    public override void Play()
+    {
+        Console.WriteLine("Бум-бум!");
+    }
+}
+
+class ProgramMusicInstruments
+{
+    static void Main()
+    {
+        Instrument[] band = { new Guitar(), new Drum(), new Instrument() };
+        foreach (var i in band)
+        {
+            i.Play();
+        }
+        Console.ReadLine();
+    }
+}
